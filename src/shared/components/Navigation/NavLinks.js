5a3e71765f234cfb342442a5c4682fa9b978cb41
anouar garbaa <g.anouar@yahoo.com>
2025-04-12 23:53:46 +0200

    Listening to Context Changes

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/auth-context";
 
-import './NavLinks.css';
+import "./NavLinks.css";
 
-const NavLinks = props => {
-  return <ul className="nav-links">
-    <li>
-      {/* "exact" tells the Nav link that this link is marked active and active css are applied only when the exact route "/" matches   */}
-      <NavLink to="/" exact>ALL USERS</NavLink>
-    </li>
-    <li>
-      <NavLink to="/u1/places">MY PLACES</NavLink>
-    </li>
-    <li>
-      <NavLink to="/places/new">ADD PLACE</NavLink>
-    </li>
-    <li>
-      <NavLink to="/auth">AUTHENTICATE</NavLink>
-    </li>
-  </ul>
+const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+
+  return (
+    <ul className="nav-links">
+      <li>
+        {/* "exact" tells the Nav link that this link is marked active and active css are applied only when the exact route "/" matches   */}
+        <NavLink to="/" exact>
+          ALL USERS
+        </NavLink>
+      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
+    </ul>
+  );
 };
 
 export default NavLinks;

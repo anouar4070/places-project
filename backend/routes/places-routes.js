d577b7addcdbf85d11de9f5d152f4a866563cf54
anouar garbaa <g.anouar@yahoo.com>
2025-04-14 00:08:47 +0200

    Handling Errors

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,36 +1,52 @@
-const express = require('express')
+const express = require("express");
 
 const router = express.Router();
 
 const DUMMY_PLACES = [
   {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
     location: {
       lat: 40.7484405,
-      lng: -73.9878584
+      lng: -73.9878584,
     },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.');
+    error.code = 404;
+    throw error; // this one trigger the error handling middleware
   }
 
-]
+  res.json({ place }); // { place: place }
+});
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId
-  })
-  res.json({place}); // { place: place }
-})
-
-router.get('/user/:uid', (req, res, next) => {
+router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find(p => {
-    return p.creator === userId
-  })
-  res.json({place}); // { place: place }
-})
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    // return res
+    //   .status(404)
+    //   .json({ message: "Could not find a place for the provided user id." });
+    const error = new Error('Could not find a place for the provided user id.');
+    error.code = 404;
+    return next(error);
+  }
+
+  res.json({ place }); // { place: place }
+});
 
 module.exports = router;

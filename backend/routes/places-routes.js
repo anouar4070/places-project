e238cfdc58afb7a07da4fd045fe3f292086ab115
anouar garbaa <g.anouar@yahoo.com>
2025-04-14 21:50:10 +0200

    adding PATCH & DELETE Routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -12,6 +12,10 @@ router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 router.post('/', placesControllers.createPlace);
 
+router.patch("/:pid", placesControllers.updatePlace);
+router.delete("/:pid", placesControllers.deletePlace);
+
+
 
 module.exports = router;
 

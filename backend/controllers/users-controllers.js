e98eebdfe076e4989a0708282d156e9f6c4e60c1
anouar garbaa <g.anouar@yahoo.com>
2025-04-20 22:51:05 +0200

    Sending a POST Request to the Backend

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,17 +1,17 @@
-const { v4: uuidv4 } = require("uuid");
+//const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Anouar Garbaa",
-    email: "test@test.com",
-    password: "testers",
-  },
-];
+// const DUMMY_USERS = [
+//   {
+//     id: "u1",
+//     name: "Anouar Garbaa",
+//     email: "test@test.com",
+//     password: "testers",
+//   },
+// ];
 
 const getUsers = async (req, res, next) => {
   let users;

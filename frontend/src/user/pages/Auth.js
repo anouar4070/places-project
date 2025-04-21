6d441e6af5adde67d983052b25da06e3b47b2e30
anouar garbaa <g.anouar@yahoo.com>
2025-04-21 12:52:46 +0200

    Sending a Login Request

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -62,10 +62,34 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
     //console.log(formState.inputs);
+    setIsLoading(true);
+
     if (isLoginMode) {
+try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+        auth.login(); //🚀 update the context & the data managed there & therefore the user interface🛠️
+      } catch (err) {
+        //console.log(err);
+        setIsLoading(false);
+        setError(err.message || "Something went wrong, please try again.");
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -82,7 +106,7 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message);
         }
-        console.log(responseData);
+        //console.log(responseData);
         setIsLoading(false);
         auth.login(); //🚀 update the context & the data managed there & therefore the user interface🛠️
       } catch (err) {
